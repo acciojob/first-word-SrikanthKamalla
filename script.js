25ab37ace873dd016709c39ce31a977745bad69f
Srikanth Kamalla <113651024+SrikanthKamalla@users.noreply.github.com>
2025-01-16 17:42:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
   // your code here
+	let i=0;
+let n=s.length;
+let t='';
+s=s.trim();
+while (i<n && s[i]!=' ') {
+    t+=s[i];
+    i++;
+}
+return t;
 }
 
 // Do not change the code below
